4fd7949fb2a5ef0607be970143bf2c85e1126654
chrisL <chris@simplifymy.cloud>
2019-03-18 15:35:37 -0700

    moved vars to tfvars - added gcp provider

---
+++ FoundationLayer/gcp_provider.tf
@@ -0,0 +1,13 @@
+provider "google" {
+  credentials = "${file("~/.config/gcloud/terraform-admin.json")}"
+  project     = "theorum"
+  region      = "us-west1"
+}
+
+terraform {
+ backend "gcs" {
+   bucket  = "smc-theorum-terraform-state-files"
+   path    = "/terraform.tfstate"
+   project = "theorum"
+ }
+}
