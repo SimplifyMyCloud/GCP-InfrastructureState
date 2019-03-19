cafc5908270eecefd879e2bfb8f34333fb08609a
chrisL <chris@simplifymy.cloud>
2019-03-18 21:41:08 -0700

    added vars to provider for envs

--- FoundationLayer/gcp_provider.tf
+++ FoundationLayer/gcp_provider.tf
@@ -1,13 +1,13 @@
 provider "google" {
   credentials = "${file("~/.config/gcloud/terraform-admin.json")}"
-  project     = "theorum"
-  region      = "us-west1"
+  project     = "${var.gcp_project}"
+  region      = "${var.gcp_region}"
 }
 
 terraform {
  backend "gcs" {
    bucket  = "smc-theorum-terraform-state-files"
-   prefix    = "/terraform.tfstate"
-   project = "theorum"
+   prefix    = "/terraform_state_${var.gcp_environment}.tfstate"
+   project = "${var.gcp_project}"
  }
 }
