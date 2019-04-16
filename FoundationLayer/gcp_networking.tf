18d704212ab270a081c9f1fe27b4e9d0102e326a
chrisL <chris@simplifymy.cloud>
2019-04-16 15:52:17 -0700

    added data gathering for default networks

--- FoundationLayer/gcp_networking.tf
+++ FoundationLayer/gcp_networking.tf
@@ -1,12 +1,35 @@
 # ---------------------------------------------------------------------------------------------------------------------
 # Foundation Layer
-# GCP networking state
+# GCP Networking state
 # ---------------------------------------------------------------------------------------------------------------------
 
 # ensure data gathered from default project
 
-data  "google_project"  "project" {}
+# GCP project & region output
+output "GCP region" {
+  value = "Terraform is targeting the ${var.gcp_region} region"
+}
 
-output  "GCP network project output"  {
-  value = "a GCP project named ${data.google_project.project.name} has the id ${data.google_project.project.number}"
+data "google_project" "default_project" {}
+
+output "GCP default project" {
+  value = "a GCP project named ${data.google_project.default_project.name} has the id ${data.google_project.default_project.number}"
+}
+
+# GCP VPC default network output
+data "google_compute_network" "default" {
+  name = "default"
+}
+
+output "GCP default VPC network" {
+  value = "a GCP default VPC network named ${data.google_compute_network.default.name}"
+}
+
+# GCP VPC default subnet output
+data "google_compute_subnetwork" "default" {
+  name = "default"
+}
+
+output "GCP default VPC subnetwork" {
+  value = "a GCP default VPC subnetwork list with the name ${data.google_compute_subnetwork.default.name} has a CIDR range of ${data.google_compute_subnetwork.default.ip_cidr_range}"
 }
