7598f261771abfceff27d62959b78908d7d500ca
chrisL <chris@simplifymy.cloud>
2019-05-20 16:47:48 -0700

    commented out backend for atlantis

--- FoundationLayer/gcp_provider.tf
+++ FoundationLayer/gcp_provider.tf
@@ -9,10 +9,10 @@ provider "google" {
   region      = "us-west1"
 }
 
-terraform {
-  backend "gcs" {
-    bucket  = "smc-terraform-state-files-dev"
-    prefix  = "/terraform_state_dev.tfstate"
-    project = "simplifymycloud-dev"
-  }
-}
+#terraform {
+#  backend "gcs" {
+#    bucket  = "smc-terraform-state-files-dev"
+#    prefix  = "/terraform_state_dev.tfstate"
+#    project = "simplifymycloud-dev"
+#  }
+#}
