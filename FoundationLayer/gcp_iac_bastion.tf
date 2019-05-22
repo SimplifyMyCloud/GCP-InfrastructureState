ac1720cdf866d437263c62fe2a35ba2f52151bb1
chrisL <chris@simplifymy.cloud>
2019-05-21 22:23:14 -0700

    added bastion for terraform

---
+++ FoundationLayer/gcp_iac_bastion.tf
@@ -0,0 +1,6 @@
+# ---------------------------------------------------------------------------------------------------------------------
+# Foundation Layer
+# GCP IAC Bastion
+# Ensures a GCE VM exists in all projects created to provide a bastion server to run Terraform 
+# and ensure the state of that project
+# ---------------------------------------------------------------------------------------------------------------------
