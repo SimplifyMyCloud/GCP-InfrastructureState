729d0a7d774badb9c8f5272ea653b5e272157ef3
chrisL <chris@simplifymy.cloud>
2019-12-31 14:58:04 -0800

    adding gcp provider for terraform

--- FoundationLayer/gcp_provider.tf
+++ FoundationLayer/gcp_provider.tf
@@ -2,9 +2,6 @@
 # Foundation Layer
 # GCP Provider
 # ---------------------------------------------------------------------------------------------------------------------
-
 provider "google" {
-#  credentials = "${file("~/.config/gcloud/terraform-admin.json")}"
-  project     = "simplifymycloud-dev"
-  region      = "us-west1"
+  region = "us-west1"
 }
