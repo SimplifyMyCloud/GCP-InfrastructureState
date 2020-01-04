777890994a7e1e9c778438f88fa69ee3b0f2f6ec
chrisL <chris@simplifymy.cloud>
2020-01-03 18:02:59 -0800

    test env backend

---
+++ FoundationLayer/GCP_Projects_Test/gcs_backend.tf
@@ -0,0 +1,10 @@
+# ---------------------------------------------------------------------------------------------------------------------
+# Foundation Layer - GCP Test Project
+# GCS Backend for Terraform State
+# ---------------------------------------------------------------------------------------------------------------------
+terraform {
+  backend "gcs" {
+    bucket  = "iq9-terraform-shared-state-bucket"
+    prefix  = "terraform/state/foundation/gcp_projects/test_env"
+  }
+}
