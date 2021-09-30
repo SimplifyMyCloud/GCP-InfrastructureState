06dba5c29f7d08dd60d9342fe1268b7db53b0c02
Chris <chris@simplifymy.cloud>
2021-09-30 15:01:49 -0700

    added top level folders - commented out lower level folders

--- FoundationLayer/GCP_Folders/gcp_folders.tf
+++ FoundationLayer/GCP_Folders/gcp_folders.tf
@@ -3,31 +3,36 @@
 # GCP Folders
 # ensures GCP Folders per Infrastructure Environment for the Foundation Layer
 # ---------------------------------------------------------------------------------------------------------------------
-module "folders" {
-  source  = "terraform-google-modules/folders/google"
-  version = "~> 2.0"
+#
+# Top level GCP Folders are to split Cloud Foundation Toolkit & Vanilla Terraform
 
-  parent  = "organizations/447686549950"
-
-  names = [
-    "dev",
-    "test",
-    "production",
-    "ops",
-    "logwarehouse",
-  ]
-
-  set_roles = true
-
-  per_folder_admins = [
-    "group:gcp-developers@iq9.io",
-    "group:gcp-test@iq9.io",
-    "group:gcp-production@iq9.io",
-    "group:gcp-ops@iq9.io",
-    "group:gcp-logwarehouse@iq9.io",
-  ]
-
-  all_folder_admins = [
-    "group:gcp-ops@iq9.io",
-  ]
+resource "google_folder" "tf_gcp_folder" {
+  display_name = "tf"
+  parent = "organizations/447686549950"
 }
+
+# Second level GCP Folders - to host vanilla Terraform
+#resource "google_folder" "sandbox_env_tf_gcp_folder" {
+#  display_name = "iq9_sandbox_tf"
+#  parent  = "organizations/447686549950"
+#}
+
+#resource "google_folder" "dev_env_tf_gcp_folder" {
+#  display_name = "iq9_dev_tf"
+#  parent  = "organizations/447686549950"
+#}
+
+#resource "google_folder" "prod_env_tf_gcp_folder" {
+#  display_name = "iq9_prod_tf"
+#  parent  = "organizations/447686549950"
+#}
+
+#resource "google_folder" "ari_vatanen_gcp_folder" {
+#  display_name = "iq9_ari_dev_tf"
+#  parent  = "folders/534726012946"
+#}
+
+#resource "google#_folder" "ari_vatanen_gcp_folder" {
+#  display_name = "iq9_ari_dev_tf"
+#  parent  = "folders/534726012946"
+#}
