ed2193c3bdfa0264da80775126cd2799aee5957b
Chris <chris@simplifymy.cloud>
2021-10-01 15:34:03 -0700

    updated provider with sa key

--- gcp_provider.tf
+++ gcp_provider.tf
@@ -3,9 +3,12 @@
 # GCP Provider
 # ---------------------------------------------------------------------------------------------------------------------
 provider "google" {
-  region  = "us-west1"
+  region      = "us-west1"
+  credentials = file("~/.config/gcloud/iq9-ops-7523-a0f4e1dbaa27.json")
+
 }
 
 provider "google-beta" {
-  region  = "us-west1"
+  region      = "us-west1"
+  credentials = file("~/.config/gcloud/iq9-ops-7523-a0f4e1dbaa27.json")
 }
