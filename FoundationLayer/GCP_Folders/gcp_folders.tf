6832dad02f7c96789d26227b00581a31edba3105
Chris <chris@simplifymy.cloud>
2021-10-02 15:53:27 -0700

    cleaned up names

--- FoundationLayer/GCP_Folders/gcp_folders.tf
+++ FoundationLayer/GCP_Folders/gcp_folders.tf
@@ -29,21 +29,21 @@ resource "google_folder" "prod_env_tf_gcp_folder" {
 
 # Third level GCP Folders - to host vanilla Terraform apps
 resource "google_folder" "dev_env_ari_gcp_folder" {
-  display_name = "iq9_ari_dev_tf"
+  display_name = "iq9_ari_vatanen_dev_tf"
   parent  = "folders/882276245846"
 }
 
 resource "google_folder" "prod_env_ari_gcp_folder" {
-  display_name = "iq9_ari_prod_tf"
+  display_name = "iq9_ari_vatanen_prod_tf"
   parent  = "folders/771423495270"
 }
 
 resource "google_folder" "dev_env_colin_gcp_folder" {
-  display_name = "iq9_colin_dev_tf"
+  display_name = "iq9_colin_mcrae_dev_tf"
   parent  = "folders/882276245846"
 }
 
 resource "google_folder" "prod_env_colin_gcp_folder" {
-  display_name = "iq9_colin_prod_tf"
+  display_name = "iq9_colin_mcrae_prod_tf"
   parent  = "folders/771423495270"
 }
