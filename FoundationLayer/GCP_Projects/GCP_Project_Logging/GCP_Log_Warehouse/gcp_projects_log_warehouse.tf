58ee46cd7f8c1285f1c4984f56b20086583642c5
Chris <chris@simplifymy.cloud>
2022-04-18 15:50:32 -0700

    gcp project naming fix

--- FoundationLayer/GCP_Projects/GCP_Project_Logging/GCP_Log_Warehouse/gcp_projects_log_warehouse.tf
+++ FoundationLayer/GCP_Projects/GCP_Project_Logging/GCP_Log_Warehouse/gcp_projects_log_warehouse.tf
@@ -8,8 +8,8 @@
 # log warehouse environment
 # ensure the GCP Project
 resource "google_project" "gcp_project_iq9_log_warehouse" {
-  name                = "iq9_log_warehouse_00"
-  project_id          = "iq9_log_warehouse_00"
+  name                = "iq9-log-warehouse-00"
+  project_id          = "iq9-log-warehouse-00"
   folder_id           = "1074889022289"
   billing_account     = "01AE65-A7583F-D9EB1A"
   skip_delete         = false
@@ -17,46 +17,46 @@ resource "google_project" "gcp_project_iq9_log_warehouse" {
 }
 
 resource "google_project_service" "cloudbilling_api" {
-  project = "iq9_log_warehouse_00"
+  project = "iq9-log-warehouse-00"
   service = "cloudbilling.googleapis.com"
 }
 
 resource "google_project_service" "oslogin_api" {
-  project = "iq9_log_warehouse_00"
+  project = "iq9-log-warehouse-00"
   service = "oslogin.googleapis.com"
 }
 
 resource "google_project_service" "iam_api" {
-  project = "iq9_log_warehouse_00"
+  project = "iq9-log-warehouse-00"
   service = "iam.googleapis.com"
 }
 
 resource "google_project_service" "serviceusage_api" {
-  project = "iq9_log_warehouse_00"
+  project = "iq9-log-warehouse-00"
   service = "serviceusage.googleapis.com"
 }
 
 resource "google_project_service" "cloudresourcemanager_api" {
-  project = "iq9_log_warehouse_00"
+  project = "iq9-log-warehouse-00"
   service = "cloudresourcemanager.googleapis.com"
 }
 
 resource "google_project_service" "storage-component_api" {
-  project = "iq9_log_warehouse_00"
+  project = "iq9-log-warehouse-00"
   service = "storage-component.googleapis.com"
 }
 
 resource "google_project_service" "logging_api" {
-  project = "iq9_log_warehouse_00"
+  project = "iq9-log-warehouse-00"
   service = "logging.googleapis.com"
 }
 
 resource "google_project_service" "clouderrorreporting_api" {
-  project = "iq9_log_warehouse_00"
+  project = "iq9-log-warehouse-00"
   service = "clouderrorreporting.googleapis.com"
 }
 
 resource "google_project_service" "monitoring_api" {
-  project = "iq9_log_warehouse_00"
+  project = "iq9-log-warehouse-00"
   service = "monitoring.googleapis.com"
 }
