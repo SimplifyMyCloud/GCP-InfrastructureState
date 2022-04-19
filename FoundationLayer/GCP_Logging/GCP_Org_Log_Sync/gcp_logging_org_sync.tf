8120b41ad71e2c9cd646b75e848c393646ce5ec7
Chris <chris@simplifymy.cloud>
2022-04-19 00:01:43 -0700

    buckets

--- FoundationLayer/GCP_Logging/GCP_Org_Log_Sync/gcp_logging_org_sync.tf
+++ FoundationLayer/GCP_Logging/GCP_Org_Log_Sync/gcp_logging_org_sync.tf
@@ -9,7 +9,7 @@
 # ensure the GCP Org log sync
 
 resource "google_logging_organization_sink" "iq9_org_log_sync" {
-  name        = "iq9_org_level_log_sync"
+  name        = "iq9-log-warehouse"
   description = "GCP iq9 Org level log sync"
   org_id      = "447686549950"
   destination = "storage.googleapis.com/iq9-log-warehouse"
