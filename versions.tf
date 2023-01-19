fb76a4b217f3a24732505257c64e5fb63fb1a26b
e30chris <chris@simplifymy.cloud>
2023-01-19 15:37:49 -0500

    cleanup versions root file

---
+++ versions.tf
@@ -0,0 +1,14 @@
+# Foundation Layer - GCP Provider
+terraform {
+  required_version = ">=0.15"
+
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = ">= 4.49.0, < 5.0"
+    }
+
+    google-beta = {
+      source =  "hashicorp/google-beta"
+      version = "~> 4.0"
+    }
